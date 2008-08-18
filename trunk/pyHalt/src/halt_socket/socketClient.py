b0f74d1d4c438fd764144d44c8e33cf53dccbcaa
jd.comment <jd.comment@f0e46dee-bb54-0410-b6ae-fbe2b2c93011>
2008-08-18 22:16:53 +0000

    first working implementation

--- trunk/pyHalt/src/halt_socket/socketClient.py
+++ trunk/pyHalt/src/halt_socket/socketClient.py
@@ -1,17 +1,26 @@
 # Echo client program
 import sys
 import socket
+import fileUtils
 
 
     # The remote host
 PORT = 8081           # The same port as used by the server
-
 host = sys.argv[2]
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, PORT))
 inputS = sys.argv[1]
-s.send(inputS)
-data = s.recv(1024)
-s.close()
-print 'Received', repr(data)
+
+if host == '0.0.0.0':
+    validClients = fileUtils.getFile()
+    for i in validClients:
+        sendMessage(i)
+        print 'sending multiple '+inputS+' commands! '+'['+i+']'
+else:
+    sendMessage(host)
+
+def sendMessage(self, host):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, PORT))
+    s.send(inputS)
+    data = s.recv(1024)
+    s.close()
+    print 'Received', repr(data)
