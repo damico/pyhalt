ca0f08b646d23f09d1f6246b3786fa785e532d65
jd.comment <jd.comment@f0e46dee-bb54-0410-b6ae-fbe2b2c93011>
2008-08-22 19:41:00 +0000

    fixed order of sendMessage() fixed fileUtils import fixed fileUtils invokation added a try for invalid hosts

--- trunk/pyHalt/src/halt_socket/socketClient.py
+++ trunk/pyHalt/src/halt_socket/socketClient.py
@@ -1,7 +1,7 @@
 # Echo client program
 import sys
 import socket
-import fileUtils
+from fileUtils import fileUtils
 
 
     # The remote host
@@ -9,18 +9,23 @@ PORT = 8081           # The same port as used by the server
 host = sys.argv[2]
 inputS = sys.argv[1]
 
-if host == '0.0.0.0':
-    validClients = fileUtils.getFile()
-    for i in validClients:
-        sendMessage(i)
-        print 'sending multiple '+inputS+' commands! '+'['+i+']'
-else:
-    sendMessage(host)
-
-def sendMessage(self, host):
+def sendMessage(host):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, PORT))
     s.send(inputS)
     data = s.recv(1024)
     s.close()
     print 'Received', repr(data)
+
+if host == '0.0.0.0':
+    fU = fileUtils()
+    validClients = fU.getFile()
+    for i in validClients:
+        print 'sending multiple '+inputS+' commands! ==> '+i
+        try:
+            sendMessage(i)
+        except:
+            print 'no server at '+i
+else:
+    sendMessage(host)
+
